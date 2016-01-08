b77a9d7e42136da1e6bdd6fabb0217b0003f35ba
Oliver Keyes <ironholds@gmail.com>
2016-01-08 11:46:35 -0800

    add further checks, unit tests

--- R/query.R
+++ R/query.R
@@ -7,6 +7,13 @@ ores_query <- function(path, ...){
   return(httr::content(result))
 }
 
+#'@title List Supported Projects
+#'@description \code{\link{list_wikis}} lists Wikimedia
+#'projects that support some or all of the ORES models.
+#'
+#'@inheritParams list_models
+#'
+#'@export
 list_wikis <- function(...){
   
   result <- ores_query("scores/", ...)
@@ -19,7 +26,7 @@ list_wikis <- function(...){
 #'how they have been trained, information about the model's accuracy and
 #'ROC, and the model's version.
 #'
-#'@param project a project. Supported projects can be obtained with
+#'@param project a Wikimedia project. Supported projects can be obtained with
 #'\code{\link{list_wikis}}.
 #'
 #'@param ... further arguments to pass to httr's GET.
@@ -43,15 +50,9 @@ list_models <- function(project, ...){
 #'whether or not an edit is considered likely, by
 #'the ORES models, to be reverted.
 #'
-#'@param project the project the edit is on. Supported
-#'projects can be found with \code{\link{list_wikis}}
-#'or \code{\link{list_models}}.
-#'
 #'@param edits a vector of edit IDs, as integers.
 #'
-#'@param ... further arguments to pass to httr's GET.
-#'
-#'@return a data.frame of five columns; \code{edit}, the
+#'@return A data.frame of five columns; \code{edit}, the
 #'edit ID, \code{project}, the project, \code{prediction},
 #'whether the model predicts that the edit will be reverted,
 #'\code{false_prob}, the probability that the model's prediction
@@ -62,12 +63,111 @@ list_models <- function(project, ...){
 #'@examples
 #'# A simple, single-diff example
 #'revert_data <- check_reverted("enwiki", 34854345)
+#'
+#'@seealso
+#'\code{\link{check_goodfaith}} to identify if a set of edits were made
+#'in good faith, and \code{\link{check_damaging}} to check if a set of edits
+#'were damaging.
+#'
+#'@inheritParams list_models
 #'@export
 check_reverted <- function(project, edits, ...){
 
   result <- lapply(edits, function(id, project, ...){
     
     data <- ores_query(path = paste0("scores/", project, "/reverted/", id),
+                       ...)
+    if("error" %in% names(data[[1]])){
+      return(data.frame(edit = names(data),
+                        project = project,
+                        prediction = NA,
+                        false_prob = NA,
+                        true_prob = NA,
+                        stringsAsFactors = FALSE))
+    }
+    return(data.frame(edit = names(data),
+                      project = project,
+                      prediction = data[[1]]$prediction,
+                      false_prob = data[[1]]$probability$false,
+                      true_prob = data[[1]]$probability$true,
+                      stringsAsFactors = FALSE))
+    
+  }, project = project)
+  
+  return(do.call("rbind", result))
+}
+
+#'@title Check Good-Faith Probability
+#'@description \code{check_goodfaith} identifies whether
+#'or not an edit was made in 'good faith' - whether it was well-intentioned,
+#'even if it is not a high-quality contribution.
+#'
+#'@return A data.frame of five columns; \code{edit}, the
+#'edit ID, \code{project}, the project, \code{prediction},
+#'whether the model predicts that the edit was made in good faith,
+#'\code{false_prob}, the probability that the model's prediction
+#'is wrong, and \code{true_prob}, the probability that the model's
+#'prediction is correct. In the event of an error (due to the edit
+#'not being available) NAs will be returned in that row.
+#'
+#'@seealso
+#'\code{\link{check_reverted}} to identify if a set of edits are likely
+#'to be reverted, and \code{\link{check_damaging}} to check if a set of edits
+#'were damaging.
+#'
+#'@inheritParams check_reverted
+#'@export
+check_goodfaith <- function(project, edits, ...){
+  
+  result <- lapply(edits, function(id, project, ...){
+    
+    data <- ores_query(path = paste0("scores/", project, "/goodfaith/", id),
+                       ...)
+    if("error" %in% names(data[[1]])){
+      return(data.frame(edit = names(data),
+                        project = project,
+                        prediction = NA,
+                        false_prob = NA,
+                        true_prob = NA,
+                        stringsAsFactors = FALSE))
+    }
+    return(data.frame(edit = names(data),
+                      project = project,
+                      prediction = data[[1]]$prediction,
+                      false_prob = data[[1]]$probability$false,
+                      true_prob = data[[1]]$probability$true,
+                      stringsAsFactors = FALSE))
+    
+  }, project = project)
+  
+  return(do.call("rbind", result))
+}
+
+#'@title Check Damaging Probability
+#'@description \code{check_damaging} identifies whether
+#'or not an edit was damaging - the type that caused actual
+#'harm to an article.
+#'
+#'@return A data.frame of five columns; \code{edit}, the
+#'edit ID, \code{project}, the project, \code{prediction},
+#'whether the model predicts that the edit was damaging,
+#'\code{false_prob}, the probability that the model's prediction
+#'is wrong, and \code{true_prob}, the probability that the model's
+#'prediction is correct. In the event of an error (due to the edit
+#'not being available) NAs will be returned in that row.
+#'
+#'@seealso
+#'\code{\link{check_goodfaith}} to identify if a set of edits were made
+#'in good faith, and \code{\link{check_reverted}} to check if a set of edits
+#'are likely to be reverted.
+#'
+#'@inheritParams check_reverted
+#'@export
+check_damaging <- function(project, edits, ...){
+  
+  result <- lapply(edits, function(id, project, ...){
+    
+    data <- ores_query(path = paste0("scores/", project, "/damaging/", id),
                        ...)
     if("error" %in% names(data[[1]])){
       return(data.frame(edit = names(data),
