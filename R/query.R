0f92d07bfe8a86e4c741acaf9d842ab6bf548796
Oliver Keyes <ironholds@gmail.com>
2016-01-08 09:58:14 -0800

    start

---
+++ R/query.R
@@ -0,0 +1,69 @@
+ores_query <- function(path, ...){
+  
+  url <- paste0("http://ores.wmflabs.org/", path)
+  ua <- httr::user_agent("ORES R Client - https://github.com/Ironholds/ores")
+  result <- httr::GET(url, ua, ...)
+  httr::stop_for_status(result)
+  return(httr::content(result))
+}
+
+list_wikis <- function(...){
+  
+  result <- ores_query("scores/", ...)
+  return(result$contexts)
+}
+
+list_models <- function(project, ...){
+  
+}
+
+#'@title Check Revert Probabilities
+#'@description \code{check_reverted} identifies
+#'whether or not an edit is considered likely, by
+#'the ORES models, to be reverted.
+#'
+#'@param project the project the edit is on. Supported
+#'projects can be found with \code{\link{list_wikis}}
+#'or \code{\link{list_models}}.
+#'
+#'@param edits a vector of edit IDs, as integers.
+#'
+#'@param ... further arguments to pass to httr's GET.
+#'
+#'@return a data.frame of five columns; \code{edit}, the
+#'edit ID, \code{project}, the project, \code{prediction},
+#'whether the model predicts that the edit will be reverted,
+#'\code{false_prob}, the probability that the model's prediction
+#'is wrong, and \code{true_prob}, the probability that the model's
+#'prediction is correct. In the event of an error (due to the edit
+#'not being available) NAs will be returned in that row.
+#'
+#'@examples
+#'# A simple, single-diff example
+#'revert_data <- check_reverted("enwiki", 34854345)
+#'@export
+check_reverted <- function(project, edits, ...){
+
+  result <- lapply(edits, function(id, project, ...){
+    
+    data <- ores_query(path = paste0("scores/", project, "/reverted/", id),
+                       ...)
+    if("error" %in% names(data[[1]])){
+      return(data.frame(edit = names(data),
+                        project = project,
+                        prediction = NA,
+                        false_prob = NA,
+                        true_prob = NA,
+                        stringsAsFactors = FALSE))
+    }
+    return(data.frame(edit = names(data),
+                      project = project,
+                      prediction = data[[1]]$prediction,
+                      false_prob = data[[1]]$probability$false,
+                      true_prob = data[[1]]$probability$true,
+                      stringsAsFactors = FALSE))
+    
+  }, project = project)
+  
+  return(do.call("rbind", result))
+}
